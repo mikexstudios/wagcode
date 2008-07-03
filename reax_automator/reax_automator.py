d4bc0ae936b7f0aa70efa3df9907b5f7e2fb6530
Michael Huynh <mike.huynh@gmail.com>
2008-07-03 21:30:53 +0000

    1. Modified control file so that string substitutions can be defined right in there.
    2. Added string sub. functionality in automator script.

--- reax_automator/reax_automator.py
+++ reax_automator/reax_automator.py
@@ -10,7 +10,7 @@ __website__ = 'http://www.mikexstudios.com'
 __copyright__ = 'General Public License (GPL)'
 
 import sys #For arguments
-import os.path #For file exist check
+import os.path #For file exist check and splitext
 
 #Automator control file:
 control_file_path = sys.argv[1]
@@ -22,6 +22,7 @@ if os.path.exists(control_file_path) == False:
 #Source the control file:
 simulation = [] #Pre-define as array
 simulation.append(dict()) #Create the first element
+string_substitutions = dict()
 #This is not the best way of reading the config file since we have to re-read
 #the whole file for each new simulation list element, but it's the most elegant 
 #way to code. 
@@ -33,6 +34,16 @@ while True:
 		continue #Re-read the execfile
 	break #Otherwise, there are no errors and we break out of this loop
 
+#Now do the replaces for our strings:
+def temp_replace_tags(in_dict):
+	global string_substitutions
+	temp_dict = dict()
+	for sim_key, sim_value in in_dict.iteritems():
+		#For each simulation control setting, we apply all of the defined
+		#substitutions to it:
+		for sub_key, sub_value in string_substitutions.iteritems():
+			temp_dict[sim_key] = sim_value.replace(sub_key, sub_value)
+	return temp_dict
+simulation = map(temp_replace_tags, simulation) #Apply the func. to each simulation list element
 
-#print initial_structure
-#print simulation
+print simulation
