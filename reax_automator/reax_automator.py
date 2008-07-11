e894e02804ecd162cf7b6e53990681b340cd8aa0
Michael Huynh <mike.huynh@gmail.com>
2008-07-11 15:49:42 +0000

    Fixed bugs in reax_automator.py. Updated control file.

--- reax_automator/reax_automator.py
+++ reax_automator/reax_automator.py
@@ -9,10 +9,11 @@ __author__ = 'Michael Huynh (mikeh@caltech.edu)'
 __website__ = 'http://www.mikexstudios.com'
 __copyright__ = 'General Public License (GPL)'
 
-import sys #For arguments
+import sys #For arguments and exit (in older python versions)
 import os #For file exist check and splitext and path stuff
 import shutil
 import time #For sleep
+import re #For regex
 
 #Arguments
 control_file_path = sys.argv[1] #Automator control file
@@ -21,7 +22,7 @@ monitor_error_threshold = 3 #Number of times errors can occur in succession duri
 
 if os.path.exists(control_file_path) == False:
 	print 'Error: '+control_file_path+' does not exist!'
-	exit(1)
+	sys.exit(1)
 
 #Source the control file:
 simulation = [] #Pre-define as array
@@ -53,25 +54,29 @@ simulation = map(temp_replace_tags, simulation) #Apply the func. to each simulat
 #print simulation
 
 #Now do stuff:
+base_folder = os.getcwd() #No trailing slash.
 last_structure_folder = os.getcwd() #No trailing slash.
 last_structure_filename = initial_structure #Assume initial_structure is same for both path and filename.
+
 for each_sim in simulation:
 	#Create output folder
 	#if os.path.isdir(each_sim['output_folder']) == False:
 	#	os.mkdir(each_sim['output_folder'])
 	
 	#Copy simulation files over. New folder will be created.
-	shutil.copytree(each_sim['simulation_folder'], each_sim['output_folder'])
+	shutil.copytree(base_folder+'/'+each_sim['simulation_folder'], each_sim['output_folder'])
 
 	#If first simulation, we copy initial structure over. Otherwise, copy last
 	#structure over.
 	shutil.copy(last_structure_filename, each_sim['output_folder']) #file will be created in directory
-
-	#Link last structure to geo. If windows, then we copy file instead of symlink:
-	#if os.name == 'nt':
-	#	shutil.copy(each_sim['output_folder']+'/'+last_structure_filename, each_sim['output_folder']+'/geo')
-	#else:
-	os.symlink(each_sim['output_folder']+'/'+last_structure_filename, each_sim['output_folder']+'/geo')
+	
+	#Change directory to the output folder so that we don't have to 
+	#keep prepending the output_folder var.
+	os.chdir(each_sim['output_folder'])
+	
+	#Link last structure to geo. Note: The src argument is relative to
+	#the path specified in the dst argument for some strange reason.
+	os.symlink(last_structure_filename, 'geo')
 	
 	#TODO: Do any extra stuff to the control file
 
@@ -80,10 +85,10 @@ for each_sim in simulation:
 	os.system('mkreaxsub '+filename_no_ext)
 
 	#Submit the simulation
-	if os.path.exists(last_structure_folder+'/reax.run'):
-		os.system('rqsub '+submit_to_cluster+' reax.run')
-		#TODO: Wait a bit:
-
+	if os.path.exists('reax.run'):
+		os.system('qsub reax.run')
+		#Wait a bit (so that info.pbs can be generated)
+		time.sleep(10) #Seconds
 	else:
 		print 'ERROR: reax.run not found!'
 		exit(1)
@@ -98,31 +103,48 @@ for each_sim in simulation:
 			
 		elif jobid_regex:
 			each_sim['job_id'] = int(jobid_regex.group(1))
-
+	
 	#Monitor the simulation until it has completed.
 	monitor_errors = 0
-	while True:
-		sq_cmd = os.popen('sq')
+	simulation_running = True
+	while simulation_running == True:
+		sq_cmd = os.popen('showq|grep $USER')
+		print 'New loop instance...'
 		#Sample line:
-		#229142                slava    Running     4 30:12:19:22  Thu Jun 26 10:15:25
+		#229142                mikeh    Running     4 30:12:19:22  Thu Jun 26 10:15:25
+		#This is the case where we only had one simulation running and
+		#that simulation is now complete. So in order to get into the
+		#for loop, we add an element to the command output.
+		if sq_cmd.readline() == '':
+			sq_cmd = ['']
+		else:
+			#There is no seek, so we reissue the command as a means
+			#to reset.
+			sq_cmd = os.popen('showq|grep $USER')
+
 		for line in sq_cmd:
 			sq_jobid_regex = re.match(r'(\d+)\s+.*', line)
-			if int(sq_jobid_regex.group(1)) == each_sim['job_id']:
+			#Need to check sq_jobid_regex first in case we didn't match.
+			if sq_jobid_regex and int(sq_jobid_regex.group(1)) == each_sim['job_id']:
 				#Job is still running. Sleep for a while.
+				print 'Simulation still running...'
 				time.sleep(60*job_id_sleep_time) #In seconds
 				monitor_errors = 0 #reset our errors
-				continue
+				break #Out of for loop
 			elif os.path.exists('fort.90') and os.path.exists('fort.71'):
 				#Simulation is complete. Breakout
+				print 'Simulation complete!'
+				simulation_running = False #Break out of while loop
 				break
 			else:
+				print 'Error in monitoring: '+str(monitor_errors)
 				#Must be an error
 				if monitor_errors >= monitor_error_threshold:
 					print 'ERROR: Unable to monitor simulation'
-					exit(1)
+					sys.exit(1)
 				else:
 					monitor_errors += 1
-					continue
+					break
 	
 	#Finishing steps for the simulation that ended to prepare it for the next simulation.
 	if os.path.exists('fort.90'):
