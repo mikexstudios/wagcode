c7ccf41f1f084c78f5af182f9f20c12f2b26dd32
Michael Huynh <mike.huynh@gmail.com>
2008-07-22 06:33:18 +0000

    Made height_density_calculator.py into a function call.

--- place_determiner/height_density_calculator.py
+++ place_determiner/height_density_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-place_determiner.py
+height_density_calculator.py
 -----------------
 '''
 __version__ = '0.1.0'
@@ -9,25 +9,9 @@ __author__ = 'Michael Huynh (mikeh@caltech.edu)'
 __website__ = 'http://www.mikexstudios.com'
 __copyright__ = 'General Public License (GPL)'
 
-#import sys #For arguments and exit (in older python versions)
-#import os #For file exist check and splitext and path stuff
-#import shutil
-#import time #For sleep
-#import re #For regex
-
-#Arguments
-number_of_liquid_molecules = 54
-number_of_surface_molecules = 1
-molecule_density = 0.010313936489926729 #(EtOH) Molecules/Angstrom
-#initial_cell_height = 50 #Angstrom
-initial_cell_dimensions = (19.49070, 17.75400, 50.0) #Angstrom
-surface_molecule_volume = 76.9205 #Volume correction factor for placing molecule on surface. Angstrom
-surface_block_height = 16.0 #Angstrom. Assume that (x, y) dim. are the same.
-
-def main():
-	global number_of_liquid_molecules, number_of_surface_molecules
-	global molecule_density
-	global initial_cell_dimensions, surface_molecule_volume
+def shrink_height_by(number_of_liquid_molecules, number_of_surface_molecules,
+		molecule_density, initial_cell_dimensions, surface_molecule_volume,
+		surface_block_height):
 	
 	empty_height = initial_cell_dimensions[2] - surface_block_height
 	#Find how much we need to shrink the z-height. Since python can't do
@@ -39,9 +23,24 @@ def main():
 		(number_of_liquid_molecules + molecule_density * surface_molecule_volume * number_of_surface_molecules) \
 		/ (molecule_density * initial_cell_dimensions[0] * initial_cell_dimensions[1])
 
+	return shrink_height_by
+
+def main():
+	#Arguments
+	number_of_liquid_molecules = 54
+	number_of_surface_molecules = 1
+	molecule_density = 0.010313936489926729 #(EtOH) Molecules/Angstrom
+	#initial_cell_height = 50 #Angstrom
+	initial_cell_dimensions = (19.49070, 17.75400, 50.0) #Angstrom
+	surface_molecule_volume = 76.9205 #Volume correction factor for placing molecule on surface. Angstrom
+	surface_block_height = 16.0 #Angstrom. Assume that (x, y) dim. are the same.
+
+	etoh_shrink_height_by = shrink_height_by(number_of_liquid_molecules, 
+		number_of_surface_molecules, molecule_density, initial_cell_dimensions,
+		surface_molecule_volume, surface_block_height)
 	#So our final height is:
-	print shrink_height_by
-	print initial_cell_dimensions[2] - shrink_height_by
+	print etoh_shrink_height_by
+	print initial_cell_dimensions[2] - etoh_shrink_height_by
 
 
 if __name__ == '__main__':
