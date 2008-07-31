b773b16fa84b6320a4c2697d04896df35ca5fa30
Michael Huynh <mike.huynh@gmail.com>
2008-07-31 08:03:42 +0000

    Added excluding capabilities and auto-generate graphs to molfra_analysis.py

--- molfra_analysis.py
+++ molfra_analysis.py
@@ -12,11 +12,13 @@ NEW: Now also outputs a tsv file with total number of molecules.
 	 into gnuplot for easy plotting of tsv data.
 
 CHANGES:
-17th April 2008 - Fixed line 147 regex to account for cases where Total Number
+0.2.0 - Added option to exclude molecules from molfra plot. Also auto-
+        generates graphs by running gnuplot.
+0.1.1 - Fixed line 147 regex to account for cases where Total Number
                   of molecules has extra whitespace.
 '''
-__version__ = '0.1.1'
-__date__ = '17 April 2008'
+__version__ = '0.2.0'
+__date__ = '30th July 2008'
 __author__ = 'Michael Huynh (mikeh@caltech.edu)'
 __website__ = 'http://www.mikexstudios.com'
 __copyright__ = 'General Public License (GPL)'
@@ -24,6 +26,11 @@ __copyright__ = 'General Public License (GPL)'
 import re
 import sys
 import string
+#optparse is a pretty awesome command line parser!
+#See: #http://docs.python.org/lib/module-optparse.html
+from optparse import OptionParser
+import os #uses path and rename stuff
+import time #for sleep
 
 #Arguments:
 molfra_file = 'molfra.out'
@@ -31,9 +38,27 @@ output_file = 'molfra.tsv'
 num_mol_file = 'nummol.tsv'
 gnuplot_file = 'molfra.gplot'
 num_mol_gplot_file = 'nummol.gplot'
+exclude_molecules = [] #Gets overridden by the command line option
 
 #Regex for the iteration-freq-molecule lines:
 num_mol_regex = re.compile('^\s+(\d+)\s+(\d+)\s+x\s+([a-zA-Z0-9]+)\s+[0-9\.]\s*');
+
+#Parse command line options
+usage = 'usage: %prog -g -e "mol1, mol2, ..."'
+parser = OptionParser(usage=usage, version='%prog '+__version__)
+parser.add_option('-e', '--exclude', dest='excludemolecules', 
+                   help='List of molecules to exclude from the molfra plot.')
+parser.add_option('-g', '--graphs', action='store_true', dest='generategraphs', 
+                   help='Automatically generate gnuplot png graphs.')
+(options, args) = parser.parse_args()
+#print options
+#print args
+
+if options.excludemolecules:
+	#Split by comma, then trim
+	exclude_molecules = [i.strip() for i in options.excludemolecules.split(',')]
+	for each_mol in exclude_molecules:
+		print 'Excluding '+each_mol
 
 ############################################################
 #Returns a list of all distinct molecules encountered in the
@@ -129,44 +154,68 @@ f.next()
 #num_molecules = [] #Keeps track of number of molecules over the iterations
 current_iteration = -1 #Init value
 mol_dict = create_empty_mol_dict(molnames)
+
 while True:
-    try:
-        line = f.next()
-    except(StopIteration):
-        break #break out of while loop if we reached end of file
+	try:
+		line = f.next()
+	except(StopIteration):
+		break #break out of while loop if we reached end of file
     
-    matches = num_mol_regex.match(line)
-    if(matches != None):
-        #print matches.group(1)
-        current_iteration = matches.group(1)
-        #We want to get this data into the dictionary
-        if(mol_dict.has_key(matches.group(3))):
-            mol_dict[matches.group(3)] = matches.group(2)
-        else:
-            #This should happen (where we haven't already encountered
-            #the key). Therefore, we should error.
-            print 'ERROR: '+matches.group(3)+' not in mol_dict!'
+	matches = num_mol_regex.match(line)
+	if(matches != None):
+		#print matches.group(1)
+		current_iteration = matches.group(1)
+		#We want to get this data into the dictionary
+		if(mol_dict.has_key(matches.group(3))):
+			mol_dict[matches.group(3)] = matches.group(2)
+		else:
+			#This shouldn't happen (where we haven't already encountered
+			#the key). Therefore, we should error.
+			#Since version 0.2.0, this will occur when we are excluding
+			#molecules. Therefore, commenting out.
+			#print 'ERROR: '+matches.group(3)+' not in mol_dict!'
+			pass
+
+	##Delete the molecules we want to exclude.
+	##print exclude_molecules
+	#for each_exclude in exclude_molecules:
+	#	try:
+	#		del mol_dict[each_exclude]
+	#		print 'Excluded '+each_exclude
+	#	except KeyError:
+	#		print 'ERROR: Molecule '+each_exclude+' could not be found, so '+\
+	#		      'not excluded'
 
     #Match number of molecules
-    matches = re.match('^\s+Total number of molecules:\s*(\d+)\s*', line);
-    if(matches != None):
-        #num_molecules.append(matches.group(1))
-        fnummol.write(str(current_iteration)+"\t"+str(matches.group(1))+"\n")
+	matches = re.match('^\s+Total number of molecules:\s*(\d+)\s*', line);
+	if(matches != None):
+		#num_molecules.append(matches.group(1))
+		fnummol.write(str(current_iteration)+"\t"+str(matches.group(1))+"\n")
     
     #The 'do' part of the loop:
-    if(re.match('^Iteration Freq.+', line)):
-        #print 'we are in do!'
-
-        #We want to: 1. Write mol_dict to output file. 2. Clear mol_dict and iter.
-        mol_dict_values = map(str, mol_dict.values()) #Assume it returns values in correct order
-        #Perhaps some thing we can improve here is to have all zero values
-        #be empty so that they aren't plotted in GNU plot
-        fout.write(str(current_iteration)+"\t"+string.join(mol_dict_values, "\t")+"\n")
-
-        #Clear values
-        mol_dict_values = []
-        mol_dict = create_empty_mol_dict(molnames)
-        current_iteration = -1
+	if(re.match('^Iteration Freq.+', line)):
+		#print 'we are in do!'
+		
+		#Delete the molecules we want to exclude.
+		#print exclude_molecules
+		for each_exclude in exclude_molecules:
+			try:
+				del mol_dict[each_exclude]
+				#print 'Excluded '+each_exclude
+			except KeyError:
+				print 'ERROR: Molecule '+each_exclude+' could not be found, so '+\
+				      'not excluded'
+		
+		#We want to: 1. Write mol_dict to output file. 2. Clear mol_dict and iter.
+		mol_dict_values = map(str, mol_dict.values()) #Assume it returns values in correct order
+		#Perhaps some thing we can improve here is to have all zero values
+		#be empty so that they aren't plotted in GNU plot
+		fout.write(str(current_iteration)+"\t"+string.join(mol_dict_values, "\t")+"\n")
+		
+		#Clear values
+		mol_dict_values = []
+		mol_dict = create_empty_mol_dict(molnames)
+		current_iteration = -1
 
 fnummol.close()
 fout.close()
@@ -174,6 +223,16 @@ f.close()
 
 print 'Total number of molecules analysis generated in '+num_mol_file+". \n"
 print 'Tab separated values file generated in '+output_file+". \n"
+
+if options.generategraphs:
+	#Run gnuplot to auto-generate png graphs:
+	os.system('gnuplot '+gnuplot_file)
+	print gnuplot_file+' graph generated!'
+	time.sleep(1)
+	os.system('gnuplot '+num_mol_gplot_file)
+	print num_mol_gplot_file+' graph generated!'
+	time.sleep(3)
+	os.system('gqview')
 
 '''
 Sample Input:
