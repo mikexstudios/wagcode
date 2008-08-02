2a0e89abdd95a77bc505ae611f9006c0adae7847
Michael Huynh <mike.huynh@gmail.com>
2008-08-02 06:42:14 +0000

    Bug fixed xmol_snapshot. Running it on a few test cases seems to give right results.

--- xmol_snapshot.py
+++ xmol_snapshot.py
@@ -2,9 +2,13 @@
 '''
 xmol_snapshot.py
 -----------------
+NOTE: There is currently no check for overbounds iteration when specifying
+      an iteration rather than percent.
+
+TODO: Add interval selection.
 '''
 __version__ = '0.1.0'
-__date__ = '31 June 2008'
+__date__ = '01 August 2008'
 __author__ = 'Michael Huynh (mikeh@caltech.edu)'
 __website__ = 'http://www.mikexstudios.com'
 __copyright__ = 'General Public License (GPL)'
@@ -16,6 +20,7 @@ from optparse import OptionParser
 import os #uses path and rename stuff
 #import time #for sleep
 import signal #For fixing grep pipe issue
+import re
 
 #Parse command line options
 usage = 'usage: %prog [iteration or percent]'
@@ -33,12 +38,16 @@ else:
 xmol_file = 'xmolout'
 #iteration_regex = re.compile(r'^\s*.+?(\d+)\s+.+')
 
+#Fixes the grep pipe error with python. See:
+#http://mail.python.org/pipermail/python-list/2004-March/255270.html
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
 def main():
 	#Figure out unique identifier string (second line of xmolout file).
 	#Get the line:
 	xmolout_f = file(xmol_file)
 	xmolout_f.readline()
-	xmolout_second_line = f.readline()
+	xmolout_second_line = xmolout_f.readline()
 	xmolout_f.close()
 	#Extract unique id part:
 	unique_id_regex = re.match(r'^\s*(.+?)\s+\d+\s+.+', xmolout_second_line)
@@ -53,9 +62,6 @@ def main():
 	try:
 		in_iter_or_percent.index('%')
 		#We have percent. Now let's try to figure out the iteration.
-		#Fixes the grep pipe error with python. See:
-		#http://mail.python.org/pipermail/python-list/2004-March/255270.html
-		signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 		#Get the final iteration
 		final_iteration_cmd = os.popen('tac '+xmol_file+' | grep "'+unique_id+'" | head -n 1')
 		final_iteration_line = final_iteration_cmd.readline()
@@ -69,14 +75,14 @@ def main():
 			sys.exit(1)
 		#Calculate what iteration the percent corresponds to:
 		iteration_percent = int(in_iter_or_percent.strip('%')) * 0.01
-		target_iteration = iteration_percent * final_iteration
+		target_iteration = int(iteration_percent * final_iteration)
 	except ValueError:
 		#No percent, just iteration
-		target_iteration = in_iter_or_percent
+		target_iteration = int(in_iter_or_percent)
 	
 	#Determine the closest iteration
 	#First, determine the iteration interval
-	second_iteration_cmd = os.popen('grep "'+unique_id+'" '+xmol_file+' | head -n 2')
+	second_iteration_cmd = os.popen('grep -n "'+unique_id+'" '+xmol_file+' | head -n 2')
 	second_iteration_cmd.readline()
 	second_iteration_line = second_iteration_cmd.readline()
 	second_iteration_cmd.close()
@@ -89,18 +95,19 @@ def main():
 	else:
 		print 'ERROR: The second iteration could not be determined.'
 		sys.exit(1)
-	#Now we calculate the closest iteration:
-	whole_iteration = target_iteration / iteration_interval
-	remainer_iteration = target_iteration % iteration_interval
-	if float(remainer_iteration)/iteration_interval >= 0.5:
-		target_iteration = whole_iteration + remainer_iteration
-	else:
-		target_iteration = whole_iteration
 	
+	#Now we calculate the closest iteration:
+	whole_snapshop_number = target_iteration / iteration_interval
+	remainer_snapshop_number = target_iteration % iteration_interval
+	if float(remainer_snapshop_number)/iteration_interval >= 0.5:
+		whole_snapshop_number += 1 #Round up
+	#There is no 0th snapshot. They all start at 1.
+	whole_snapshop_number += 1 
+
 	#Now slice from that iteration
-	end_line = (target_iteration / iteration_interval) * iteration_line_interval
-	xmol_slice_cmd = os.popen('head -n '+str(end_line)+' '+mol_file+ \
-		' | tail -n '+iteration_line_interval)
+	end_line = whole_snapshop_number * iteration_line_interval
+	xmol_slice_cmd = os.popen('head -n '+str(end_line)+' '+xmol_file+ \
+		' | tail -n '+str(iteration_line_interval))
 	for line in xmol_slice_cmd:
 		print line
 	xmol_slice_cmd.close()
