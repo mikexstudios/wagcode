c46897dbb5ff287b189f6436b7822c1f3bcd06fe
Michael Huynh <mike.huynh@gmail.com>
2008-08-06 04:08:53 +0000

    Added test files.
    Added reax_connection_table.py which is a class to wrap around fort.7
    
    Working on looping part of reax_rdf code. Realized I needed to optimize the loop so I'm checking in before I make the big change.

---
+++ reax_rdf/reax_connection_table.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+'''
+reax_connection_table.py
+-----------------
+A class to represent ReaxFF's fort.7 connection table. See the ReaxFF manual
+for more info.
+'''
+__version__ = '0.1.0'
+__date__ = '05 August 2008'
+__author__ = 'Michael Huynh (mikeh@caltech.edu)'
+__website__ = 'http://www.mikexstudios.com'
+__copyright__ = 'General Public License (GPL)'
+
+#import math
+
+class Connection_Table:
+	rows = []
+
+	def __init__(self):
+		pass
+	
+	def load(self, in_file):
+		f = file(in_file)
+		#Make the 0th row nothing. We start from 1 so that it matches the numbering
+		#of the atoms
+		self.rows.append(None) 
+		#Discard the first line
+		f.next()
+		for line in f:
+			fields = line.split()
+			fields = fields[:8] #Want only the first 8 fields
+			fields = map(str.strip, fields) #trim whitespace
+			try:
+				fields = map(int, fields) #all the fields are integers
+			except ValueError:
+				#We are probably on the last line where the values are floats
+				continue #Skip to the next for loop
+			
+			#Put the connection part into a sub array so that it's easier to access:
+			temp_fields = [fields[1]] #Put in a list. This is the atom "number" (ffield) part.
+			temp_fields.append(fields[2:-1]) #This is the connections part
+			temp_fields.append(fields[-1]) #This is the molecule number part
+
+			self.rows.append(temp_fields) #maybe we want to append fields as tuple
+		f.close()
+		#print self.rows
+
+def main():
+	connect_table = Connection_Table()
+	connect_table.load('fort.7')
+
+if __name__ == '__main__':
+	main()
+
--- reax_rdf/reax_rdf.py
+++ reax_rdf/reax_rdf.py
@@ -15,6 +15,7 @@ import os #For file exist check and splitext and path stuff
 #import time #For sleep
 import re #For regex
 from XYZ import XYZ #XYZ class
+from reax_connection_table import Connection_Table
 
 #Arguments
 control_file= sys.argv[1] #Settings for RDF
@@ -29,18 +30,55 @@ print 'Read control file successfully: '+control_file
 
 def main():
 	#Read in XYZ file. Store all the coordinates.
+	simulation_atoms = XYZ()
+	simulation_atoms.load(structure_file) #simulation_atoms.rows contains the data
+	
+	#Pre-sort the atoms by atom type (into a dictionary). This way, we don't
+	#have to loop through the whole file every time we calculate distances for
+	#a given atom.
+	simulation_atoms_dict = {}
+	for atom_number, each_row in enumerate(simulation_atoms.rows):
+		#Correction to the atom_number since it starts at 1 instead of 0:
+		atom_number += 1
+		
+		#We want our new list to be in the format:
+		#atom_number x y z
+		temp_list = [atom_number] #Put it in a list first. For some reason, 
+		temp_list.extend(each_row[1:]) #can't combine these two on same line.
+		
+		#Now save it:
+		try:
+			simulation_atoms_dict[each_row[0]].append(temp_list)
+		except KeyError:
+			#This means that the dictionary entry for this atom has not been
+			#created yet. So we create it:
+			simulation_atoms_dict[each_row[0]] = [temp_list] #New list
+	
+	#Read in connection table (ReaxFF fort.7 style, see ReaxFF manual for more info)
+	connect_table = Connection_Table()
+	connect_table.load(connection_table_file)
 
 	#Loop through each pair of atoms.
-	
-		#Calculate interatomic distances. Use the minimum image convention here to
-		#take care of any periodic conditions. Since we are in this loop
+	for each_row in simulation_atoms.rows:
+		if each_row[0] == from_atom:
+			#Calculate interatomic distances. Use the minimum image convention here to
+			#take care of any periodic conditions. We start by checking through all the
+			#other atoms to see if they match our to_atom:
+			for each_target_row in simulation_atoms.rows:
+				if each_target_row[0] == to_atom:
+					#Make sure this to_atom isn't part of the same molecule. We figure
+					#this out by using the connection table:
+					if connection_table.rows[each_row
+						
+						
+				 
 
 	#Normalize the histrogram by comparing to ideal gas.
 
 
 
 def get_minimum_image_distance(in_coord):
-
+	pass
 
 if __name__ == '__main__':
 	main()
