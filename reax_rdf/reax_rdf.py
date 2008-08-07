5e41b1c84a8f5adb2f276dccda08d5c694e0f1a8
Michael Huynh <mike.huynh@gmail.com>
2008-08-07 22:58:07 +0000

    Completed all the steps to calculate g(r) in reax_rdf.py, but still need to verify that it works.
    Added output file option in test.rdfin.

--- reax_rdf/reax_rdf.py
+++ reax_rdf/reax_rdf.py
@@ -29,6 +29,9 @@ except IOError:
 	sys.exit(1)
 print 'Read control file successfully: '+control_file
 
+#Make sure some of our variables are in the correct format:
+bin_size = float(bin_size)
+
 def main():
 	#Read in XYZ file. Store all the coordinates.
 	simulation_atoms = XYZ()
@@ -62,13 +65,11 @@ def main():
 	#Loop through each pair of atoms.
 	distance_histogram = {} #Dictionary instead of array so we can add entries at any element.
 	for from_atom_row in simulation_atoms_dict[from_atom]:
-		print from_atom_row
 		#Calculate interatomic distances. Use the minimum image convention here to
 		#take care of any periodic conditions. We start by checking through all the
 		#other atoms to see if they match our to_atom:
 		try:
 			for to_atom_row in simulation_atoms_dict[to_atom]:
-				print to_atom_row
 				#Make sure this to_atom isn't part of the same molecule. We figure
 				#this out by using the molecule column of the connection table:
 				if connection_table.rows[from_atom_row[0]][2] == \
@@ -79,16 +80,17 @@ def main():
 				from_atom_coord = (from_atom_row[1], from_atom_row[2], from_atom_row[3])
 				to_atom_coord = (to_atom_row[1], to_atom_row[2], to_atom_row[3])
 				interatomic_distance = calc_interatomic_distance(from_atom_coord, to_atom_coord)
-				print interatomic_distance
-				exit(0)
+				#print interatomic_distance
+				
 				#Now figure out which bin this distance goes in. The algorithm in 
 				#Simulation of Liquids by Allen (p182) differs from the algorithm in
 				#Understanding Molecular Simulation by Frenkel (p86) in that Allen rounds
 				#up (by adding 1) while Frenkel doesn't round up. Adri's RDF script
 				#follows Allen's method. I will take the middle road and round the number:
 				#(NOTE: This method is used because FORTRAN only had arrays so the bins
-				#       have to be integers. But we don't necessarily have to do it this
-				#       way since we have dictionaries.)
+				#       have to be integers. We do it in this case because floats can't
+				#       be exactly represented in any prog. language. So this int method
+				#       is actually not too bad.
 				distance_bin = int(round(interatomic_distance / bin_size))
 				try:
 					#We add two for the contribution of both atoms because we just want
@@ -103,9 +105,45 @@ def main():
 			print 'ERROR: '+to_atom+' was not found in the structure file: '+structure_file
 			sys.exit(1)
 	
-	print distance_histogram
+	#print distance_histogram
 
 	#Normalize the histrogram by comparing to ideal gas.
+	#We want to compute (p184 Allen): 
+	# g(r + 1/2 * delta_r) = n(b)/n_id(b) , no clue what b is though
+	#where according to p183 Allen and corroborated by p86 Frenkel and Adri's RDF script:
+	# n(b) = n_his(b)/(N * t_run)
+	#In our case, we are only calculating from one static frame, so t_run = 1. The reason
+	#why we divide by N is to get the "average". It sort of doesn't make sense to me since
+	#this would make n(b) always fractional. But if n_id(b) is also fractional, then it
+	#would work out.
+	#Also according to p184 Allen:
+	# n_id(b) = (4 pi rho)/3 * [(r + dr)^3 - r^3]
+	#which makes sense. NOTE: To convert from the bin index to length (in angstroms) we 
+	#reverse what we do by multiplying by the bin_size (aka delta_r).
+	#Also, rho (the number density) is defined by wikipedia and MatDL wiki as:
+	# rho = N/V ; (num of particles)/(volume of system)
+	#But Adri's RDF script doesn't calculate rho this way. He does some sort of ratio
+	#of the two atom populations...
+	total_number_atoms = len(simulation_atoms.rows)
+	total_volume = float(unit_cell[0]) * unit_cell[1] * unit_cell[2]
+	rho = total_number_atoms/total_volume
+	g = {} #This is our pair correlation function results. However, have to store as bins, not angstroms
+	for distance_bin, n_his in distance_histogram.iteritems():
+		n = float(n_his)/total_number_atoms
+		r = float(distance_bin) * bin_size #convert from bin index to length (angstroms)
+		n_id = ((4 * math.pi * rho)/3) * ( (r + bin_size)**3 - r**3 )
+		#print n, n_id
+		g[distance_bin] = n/n_id
+	
+	#Now print it out with angstroms!
+	print "r \t g(r)"
+	for distance_bin, gr in g.iteritems():
+		#The format syntax is that we have _____._____ (5 spots before the . and 5 spots
+		#after). This will cover like 99.99% of all numbers we encounter.
+		print "%10.5f \t %10.5f" % (float(distance_bin) * bin_size, gr)
+		#pass
+	
+	#Alternative method for printing it out:
 
 
 
@@ -120,10 +158,8 @@ def calc_interatomic_distance(in_coord_a, in_coord_b):
 	#Check to see if we have periodic conditions so that
 	#we need to use the minimum image distance method.
 	if type(unit_cell) == type(()) and unit_cell != False:
-		print 'minimum image'
 		(dx, dy, dz) = get_minimum_image_distance_each_axis((dx, dy, dz))
 	
-	print (dx, dy, dz)
 	#Compute the distance
 	return math.sqrt(dx**2 + dy**2 + dz**2)
 
