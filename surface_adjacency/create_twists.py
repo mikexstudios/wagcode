fc82faa18e3498b601615091bc1c30e2a1eb4129
Michael Huynh <mike.huynh@gmail.com>
2008-08-20 05:43:06 +0000

    Updated surface_adjacency to use hole scoring algorithm. Updated create_twists.py to fix recursion problem so that pretty_output will work.

--- surface_adjacency/create_twists.py
+++ surface_adjacency/create_twists.py
@@ -17,49 +17,60 @@ import math #for ceil
 
 #Arguments
 
-def create_twists(surf_rep):
-	'''We assume that the horizontal rows have the steric clash.'''
-	#If row is odd, then there will be infinite recursion. So we'll
-	#check for that first.
-	if len(surf_rep[0]) % 2 == 1: #There is a remainder after divided by 2
-		print 'ERROR: The surface representation row length is ODD. This'+ \
-				'means that you cannot twist everything so that there are'+ \
-				'no steric horizontal clashes'
-		return surf_rep
+#Used to break out of nested for loops. See:
+#http://mail.python.org/pipermail/python-list/2002-January/123692.html
+class BreakLoop(Exception): pass 
 
-	no_clashes = True #If it's false until the end, then we just return.
-	for row_index, row in enumerate(surf_rep):
-		for spot_index, each_spot in enumerate(row):
-			if each_spot == 1 or each_spot == -1:
-				#Check the spot to the current position's right. We use modulus 
-				#to create the periodic wrap around effect. 
-				#ie. 0%2=0, 1%2=1, 2%2=0, 3%2=1, 4%2=0.
-				if surf_rep[row_index][(spot_index+1) % len(row)] == each_spot:
-					#print row_index, spot_index
-					no_clashes = False
-					#If the spot to the right is the same, then we need to invert
-					#that whole column.
-					for i in xrange(len(surf_rep)):
-						surf_rep[i][(spot_index+1) % len(row)] *= -1
-					#print surf_rep
-	
-	if no_clashes == False:
-		#We run it through another check
-		print 'Doing another pass...'
-		return create_twists(surf_rep)
-		pass
-	
-	#Otherwise, everything is fine:
-	return surf_rep
+def create_twists(surf_rep):
+    '''We assume that the horizontal rows have the steric clash.'''
+    #If row is odd, then there will be infinite recursion. So we'll
+    #check for that first.
+    if len(surf_rep[0]) % 2 == 1: #There is a remainder after divided by 2
+        print 'ERROR: The surface representation row length is ODD. This'+ \
+                'means that you cannot twist everything so that there are'+ \
+                'no steric horizontal clashes'
+        return surf_rep
+
+    no_clashes = True #If it's false until the end, then we just return.
+    try:
+        for row_index, row in enumerate(surf_rep):
+            for spot_index, each_spot in enumerate(row):
+                if each_spot == 1 or each_spot == -1:
+                    #Check the spot to the current position's right. We use modulus 
+                    #to create the periodic wrap around effect. 
+                    #ie. 0%2=0, 1%2=1, 2%2=0, 3%2=1, 4%2=0.
+                    if surf_rep[row_index][(spot_index+1) % len(row)] == each_spot:
+                        #print row_index, spot_index
+                        no_clashes = False
+                        #If the spot to the right is the same, then we need to invert
+                        #that whole column.
+                        for i in xrange(len(surf_rep)):
+                            surf_rep[i][(spot_index+1) % len(row)] *= -1
+                        #print surf_rep
+                        #Now that we made a change, let's go back to the beginning
+                        #and check everything again:
+                        raise BreakLoop
+    except BreakLoop:
+        pass #Do nothing, since we set no_clashes to False, we will recurse
+
+    
+    if no_clashes == False:
+        #We run it through another check
+        print 'Doing another pass...'
+        return create_twists(surf_rep)
+        pass
+    
+    #Otherwise, everything is fine:
+    return surf_rep
 
 
 
 def main():
-	etoh18 = [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
-	etoh18 = [[0, 1, 0, 1, 0, 1], [1, 0, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
-	etoh18 = [[0, 1, 0, 1, 0, 1], [0, 1, 1, 0, 1, 1], [1, 0, 1, 1, 1, 0]]
-	#etoh18 = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
-	print create_twists(etoh18)
+    etoh18 = [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
+    etoh18 = [[0, 1, 0, 1, 0, 1], [1, 0, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
+    etoh18 = [[0, 1, 0, 1, 0, 1], [0, 1, 1, 0, 1, 1], [1, 0, 1, 1, 1, 0]]
+    #etoh18 = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    print create_twists(etoh18)
 
 if __name__ == '__main__':
-	main()
+    main()
--- surface_adjacency/pretty_output.py
+++ surface_adjacency/pretty_output.py
@@ -22,80 +22,80 @@ max_horizontal_spots = 6 #This allows us to convert the long string into an arra
 db_file = 'surface_adjacency.db'
 
 def convert_bin_string_to_list(bin_string):
-	global max_horizontal_spots
-	
-	max_iter = len(bin_string)/float(max_horizontal_spots)
-	max_iter = int(math.ceil(max_iter)) #We want to round up so that our loop goes through the full string
-	bin_list= []
-	bin_string = list(bin_string) #We put each character in its own list element
-	bin_string = map(int, bin_string) #Convert each element to int
-	
-	#Note for this method, it doesn't necessarily return a rectangular 2D list.
-	for i in range(max_iter):
-		bin_list.append(bin_string[0:max_horizontal_spots])
-		bin_string = bin_string[max_horizontal_spots:] #Rest of the list
-	
-	return bin_list
+    global max_horizontal_spots
+    
+    max_iter = len(bin_string)/float(max_horizontal_spots)
+    max_iter = int(math.ceil(max_iter)) #We want to round up so that our loop goes through the full string
+    bin_list= []
+    bin_string = list(bin_string) #We put each character in its own list element
+    bin_string = map(int, bin_string) #Convert each element to int
+    
+    #Note for this method, it doesn't necessarily return a rectangular 2D list.
+    for i in range(max_iter):
+        bin_list.append(bin_string[0:max_horizontal_spots])
+        bin_string = bin_string[max_horizontal_spots:] #Rest of the list
+    
+    return bin_list
 
 def transpose_list(lists):
    if not lists: return []
    return map(lambda *row: list(row), *lists)
 
 def main():
-	global output_file
-	global db_file
-	global db
-	
-	#Connect to our sqlite database. 
-	if os.path.isfile(db_file):
-		conn = sqlite3.connect(db_file)
-		conn.row_factory = sqlite3.Row #Allows us to access rows by column name
-		db = conn.cursor()
-	else:
-		print 'Database file not found: '+db_file
-		exit()
-	
-	f = file(output_file, 'w')
-	#f.write("eto_molecule_placements = [\n")
-	#Make a table of the best score in each category:
-	for each_num_mol in range(0,19): #0 to 18
-		#db.execute('SELECT * FROM surf_configuration WHERE num_mol = ? ORDER BY score DESC LIMIT 1', (each_num_mol,))
-		db.execute('''
-			SELECT * FROM surf_configuration
-		    WHERE num_mol = ?
-			ORDER BY score ASC
-			LIMIT 1
-			''', (each_num_mol,))
-		row = db.fetchone()
-		
-		#Uncomment if want twisted EtOH:
-		twisted_rep = create_twists.create_twists( \
-			convert_bin_string_to_list(row['config']) \
-			)
-		#Comment out below since we want 6 in horizontal
-		#twisted_rep = transpose_list(twisted_rep)
-		#Now go in and do string replaces for -1 entries:
-		#twisted_rep = str(twisted_rep)
-		#twisted_rep = twisted_rep.replace('-1', "[1,5,'z',180]")
-		#f.write(twisted_rep+", \n")
+    global output_file
+    global db_file
+    global db
+    
+    #Connect to our sqlite database. 
+    if os.path.isfile(db_file):
+        conn = sqlite3.connect(db_file)
+        conn.row_factory = sqlite3.Row #Allows us to access rows by column name
+        db = conn.cursor()
+    else:
+        print 'Database file not found: '+db_file
+        exit()
+    
+    f = file(output_file, 'w')
+    #f.write("eto_molecule_placements = [\n")
+    #Make a table of the best score in each category:
+    for each_num_mol in range(0,19): #0 to 18
+        #db.execute('SELECT * FROM surf_configuration WHERE num_mol = ? ORDER BY score DESC LIMIT 1', (each_num_mol,))
+        db.execute('''
+            SELECT * FROM surf_configuration
+            WHERE num_mol = ?
+            ORDER BY score ASC
+            LIMIT 1
+            ''', (each_num_mol,))
+        row = db.fetchone()
+        
+        #Uncomment if want twisted EtOH:
+        twisted_rep = create_twists.create_twists( \
+            convert_bin_string_to_list(row['config']) \
+            )
+        #Comment out below since we want 6 in horizontal
+        #twisted_rep = transpose_list(twisted_rep)
+        #Now go in and do string replaces for -1 entries:
+        #twisted_rep = str(twisted_rep)
+        #twisted_rep = twisted_rep.replace('-1', "[1,5,'z',180]")
+        #f.write(twisted_rep+", \n")
 
-		f.write(str(each_num_mol)+" EtOH: \n")
-		for row in twisted_rep:
-			for each_spot in row:
-				if each_spot == -1:
-					each_spot = 2 #Do this for now so that ASCII output can be all aligned
-				f.write(str(each_spot))
-			f.write("\n")
-		f.write("\n")		
-		
-		#Uncomment if want non-twisted EtOH:
-		#f.write(str(convert_bin_string_to_list(row['config']))+", \n")
+        f.write(str(each_num_mol)+" EtOH: \n")
+        for row in twisted_rep:
+            for each_spot in row:
+                if each_spot == -1:
+                    each_spot = 2 #Do this for now so that ASCII output can be all aligned
+                f.write(str(each_spot))
+            f.write("\n")
+        f.write("\n")       
+        
+        #Uncomment if want non-twisted EtOH:
+        #f.write(str(convert_bin_string_to_list(row['config']))+", \n")
 
-	#f.write("]\n")
-	f.close()
-	
-	db.close()
-	conn.close()
+    #f.write("]\n")
+    f.close()
+    
+    db.close()
+    conn.close()
 
 if __name__ == '__main__':
-	main()
+    main()
