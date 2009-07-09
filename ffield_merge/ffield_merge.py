a7445a016e88f95278de5335b52d9f09ef56c16b
Michael Huynh <mike.huynh@gmail.com>
2009-07-08 23:12:53 -0700

    Parsing atom and bond sections of ffield to a dict works.
    Added some .gitignore files to take care of test files.

--- ffield_merge/ffield_merge.py
+++ ffield_merge/ffield_merge.py
@@ -4,131 +4,256 @@
 ffield_merge.py
 -----------------
 Merges two torsions part of a ffield file.
-
-$Id: reax_molfra.py 128 2008-09-23 11:02:20Z mikexstudios $
 '''
 __author__ = 'Michael Huynh (mikeh@caltech.edu)'
 __website__ = 'http://www.mikexstudios.com'
 __copyright__ = 'General Public License (GPL)'
 
-import sys #For arguments and exit (in older python versions)
-import os #For file exist check and splitext and path stuff
+#import sys #For arguments and exit (in older python versions)
+#import os #For file exist check and splitext and path stuff
 import re
-import time #for sleep
+#import time #for sleep
+import itertools
 
-base_text = '''
-  1  1  1  1  -0.2500  34.7453   0.0288  -6.3507  -1.6000   0.0000   0.0000     
-  1  1  1  2  -0.2500  29.2131   0.2945  -4.9581  -2.1802   0.0000   0.0000     
-  2  1  1  2  -0.2500  31.2081   0.4539  -4.8923  -2.2677   0.0000   0.0000     
-  1  1  1  3   1.2799  20.7787  -0.5249  -2.5000  -1.0000   0.0000   0.0000     
-  2  1  1  3   1.9159  19.8113   0.7914  -4.6995  -1.0000   0.0000   0.0000     
-  3  1  1  3  -1.4477  16.6853   0.6461  -4.9622  -1.0000   0.0000   0.0000     
-  1  1  3  1   0.4816  19.6316  -0.0057  -2.5000  -1.0000   0.0000   0.0000     
-  1  1  3  2   1.2044  80.0000  -0.3139  -6.1481  -1.0000   0.0000   0.0000     
-  2  1  3  1  -2.5000  31.0191   0.6165  -2.7733  -2.9807   0.0000   0.0000     
-  2  1  3  2  -2.4875  70.8145   0.7582  -4.2274  -3.0000   0.0000   0.0000     
-  1  1  3  3  -0.3566  10.0000   0.0816  -2.6110  -1.9631   0.0000   0.0000     
-  2  1  3  3  -1.4383  80.0000   1.0000  -3.6877  -2.8000   0.0000   0.0000     
-  3  1  3  1  -1.1390  78.0747  -0.0964  -4.5172  -3.0000   0.0000   0.0000     
-  3  1  3  2  -2.5000  70.3345  -1.0000  -5.5315  -3.0000   0.0000   0.0000     
-  3  1  3  3  -2.0234  80.0000   0.1684  -3.1568  -2.6174   0.0000   0.0000     
-  1  3  3  1   1.1637 -17.3637   0.5459  -3.6005  -2.6938   0.0000   0.0000     
-  1  3  3  2  -2.1289  12.8382   1.0000  -5.6657  -2.9759   0.0000   0.0000     
-  2  3  3  2   2.5000 -22.9397   0.6991  -3.3961  -1.0000   0.0000   0.0000     
-  1  3  3  3   2.5000 -25.0000   1.0000  -2.5000  -1.0000   0.0000   0.0000     
-  2  3  3  3  -2.5000  -2.5103  -1.0000  -2.5000  -1.0000   0.0000   0.0000     
-  3  3  3  3  -2.5000 -25.0000   1.0000  -2.5000  -1.0000   0.0000   0.0000     
-  0  1  2  0   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000     
-  0  2  2  0   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000     
-  0  2  3  0   0.0000   0.1000   0.0200  -2.5415   0.0000   0.0000   0.0000     
-  0  1  1  0   0.0000  50.0000   0.3000  -4.0000  -2.0000   0.0000   0.0000     
-  0  3  3  0   0.5511  25.4150   1.1330  -5.1903  -1.0000   0.0000   0.0000     
-  0  1  4  0   1.7932 141.5515   0.9686  -4.2368  -1.9727   0.0000   0.0000     
-  0  2  4  0  -1.5000   0.1032   0.0100  -5.0965   0.0000   0.0000   0.0000     
-  0  3  4  0   1.1397  61.3225   0.5139  -3.8507  -3.0000   0.0000   0.0000     
-  0  4  4  0   0.7265  44.3155   1.0000  -4.4046  -2.0000   0.0000   0.0000     
-  4  1  4  4  -0.0949   8.7582   0.3310  -7.9430  -2.0000   0.0000   0.0000     
-  0  1  5  0   3.3423  30.3435   0.0365  -2.7171   0.0000   0.0000   0.0000     
-  0  5  5  0  -0.0555  -5.0000   0.1515  -2.2056   0.0000   0.0000   0.0000     
-  0  2  5  0   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000     
-  2  3  5  3   2.5000   2.5000   0.2237 -10.0000  -1.0000   0.0000   0.0000     
-  0  3  5  0  -2.5000  50.0000  -0.5000 -10.0000  -1.0000   0.0000   0.0000     
-  0  6  6  0   0.0000   0.0000   0.1200  -2.4426   0.0000   0.0000   0.0000     
-  0  2  6  0   0.0000   0.0000   0.1200  -2.4847   0.0000   0.0000   0.0000     
-  0  3  6  0   0.0000   0.0000   0.1200  -2.4703   0.0000   0.0000   0.0000     
-  1  1  3  3  -0.0002  20.1851   0.1601  -9.0000  -2.0000   0.0000   0.0000     
-  1  3  3  1   0.0002  80.0000  -1.5000  -4.4848  -2.0000   0.0000   0.0000     
-  3  1  3  3  -0.1583  20.0000   1.5000  -9.0000  -2.0000   0.0000   0.0000     
-  1  1  1  7  -0.3232  14.3871   0.1823  -9.8682  -1.7255   0.0000   0.0000     
-  7  1  1  7  -0.1452  50.0000  -0.1915  -8.0773  -1.7255   0.0000   0.0000     
-  0  1  7  0   4.0000  45.8264   0.9000  -4.0000   0.0000   0.0000   0.0000     
-  0  7  7  0   4.0000  45.8264   0.9000  -4.0000   0.0000   0.0000   0.0000     
-  2  1  3  7  -1.5000  18.9285   0.3649  -6.1208   0.0000   0.0000   0.0000     
-  2  3  7  3   1.5000  -1.0000   0.2575  -6.2100   0.0000   0.0000   0.0000     
-  1  3  7  3  -1.4375  -0.8700   0.9861  -2.5424   0.0000   0.0000   0.0000     
-  7  3  7  3  -1.5000  21.5086  -1.0000  -4.8869   0.0000   0.0000   0.0000     
-  2  1  3  9   0.0000  84.3556   0.1000  -3.1953   0.0000   0.0000   0.0000     
-  1  1  3  9   0.0000  51.0461   0.1059  -7.2043   0.0000   0.0000   0.0000     
-  2  3  9  3   0.0000  14.7459   0.1000  -3.0965   0.0000   0.0000   0.0000     
-  1  1  1 11   0.5000   0.1000   0.4683 -11.5274  -1.7255   0.0000   0.0000     
-  2  1  1 11   0.0000  49.3871   0.2000 -10.5765  -1.7255   0.0000   0.0000     
- 11  1  1 11  -0.5000  95.4727  -0.2080  -4.8579  -1.7255   0.0000   0.0000     
-  0  1 11  0   4.0000  45.8264   0.9000  -4.0000   0.0000   0.0000   0.0000     
-  0 11 11  0   4.0000  45.8264   0.9000  -4.0000   0.0000   0.0000   0.0000     
-'''
+from_ffield = 'ffield_v-bi-ti-mo-ruN'
+to_ffield   = 'ffield_ti-o-h-na-cl-s-p'
+move_atoms = ('C', 'H', 'O', 'N', 'S')
 
-override_text = '''
-  1  1  1  1  -0.2500  34.7453   0.0288  -6.3507  -1.6000   0.0000   0.0000     
-  1  1  1  2  -0.2500  29.2131   0.2945  -4.9581  -2.1802   0.0000   0.0000     
-  2  1  1  2  -0.2500  31.2081   0.4539  -4.8923  -2.2677   0.0000   0.0000     
-  0  1  2  0   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000     
-  0  2  2  0   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000     
-  0  1  3  0   0.0064  92.1459   0.9925  -3.0659  -2.1565   0.0000   0.0000     
-  0  2  3  0   0.0000   0.1000   0.0200  -2.5415   0.0000   0.0000   0.0000     
-  0  3  3  0   0.5511  25.4150   1.1330  -5.1903  -1.0000   0.0000   0.0000     
-  0  1  4  0  -2.4242 128.1636   0.3739  -6.6098  -2.0000   0.0000   0.0000     
-  0  2  4  0   0.0000   0.1000   0.0200  -2.5415   0.0000   0.0000   0.0000     
-  0  3  4  0   1.4816  55.6641   0.0004  -7.0465  -2.0000   0.0000   0.0000     
-  0  4  4  0  -0.3244  27.7086   0.0039  -2.8272  -2.0000   0.0000   0.0000     
-  0  1  1  0   0.0000   0.6675   0.0000  -8.2352   0.0000   0.0000   0.0000     
-  4  1  4  4  -5.5181   8.9706   0.0004  -6.1782  -2.0000   0.0000   0.0000     
-  0  1  5  0   3.3423  30.3435   0.0365  -2.7171   0.0000   0.0000   0.0000     
-  0  5  5  0  -0.0555 -42.7738   0.1515  -2.2056   0.0000   0.0000   0.0000     
-  0  2  5  0   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000   0.0000     
-  0  6  6  0   0.0000   0.0000   0.1200  -2.4426   0.0000   0.0000   0.0000     
-  0  2  6  0   0.0000   0.0000   0.1200  -2.4847   0.0000   0.0000   0.0000     
-  0  3  6  0   0.0000   0.0000   0.1200  -2.4703   0.0000   0.0000   0.0000     
-  1  3  3  1   3.0311  96.8000   0.6062  -4.3299  -2.0536   0.0000   0.0000     
-  1  1  3  1   0.0588 100.0000   0.0161  -2.5000  -1.0000   0.0000   0.0000     
-  2  1  3  1   4.0000  25.0000   0.8942  -9.0000  -1.1657   0.0000   0.0000     
-  1  1  3  2   4.0000 100.0000   2.0000  -2.5000  -1.0000   0.0000   0.0000     
-  2  1  3  2   2.4070 100.0000   1.5438  -5.3254  -2.7319   0.0000   0.0000     
-  2  1  3 14   1.6297  56.8132   0.3398  -2.6912  -2.1000   0.0000   0.0000     
-  1  1  3 14  -0.0427  13.4096   0.9351  -6.5245  -2.1000   0.0000   0.0000     
-  2  3 14  3   2.5000  11.6208   1.0000  -9.0000  -1.0000   0.0000   0.0000     
-  2  1  3 12  -0.2500  45.7639   0.3000  -3.5745  -2.1565   0.0000   0.0000     
-  1  1  3 12  -0.2500  69.1094   0.3000  -3.0983  -2.1565   0.0000   0.0000     
-  2  3 12  3  -0.4306   7.5000  -0.5000  -6.9948  -1.0000   0.0000   0.0000     
-  2  1  3 13  -0.0131  90.0000   0.1642  -3.8725  -2.1565   0.0000   0.0000     
-  1  1  3 13   0.1646  87.8037   0.1000  -3.1250  -2.1565   0.0000   0.0000     
-  2  3 13  3   2.5000   1.0000   0.7530  -9.0000  -1.0000   0.0000   0.0000     
-'''
 
 def main():
-    #Parse text into dict-line
-    base_dict = text_to_dict(base_text.splitlines())
-    #print str(len(base_dict))
-    override_dict = text_to_dict(override_text.splitlines())
+    #Parse file into each section
+    #from_f = file(from_ffield)
+    #from_sections = parse_sections(from_f)
 
-    #Merge dicts
-    merged_dict = merge_dicts(base_dict, override_dict)
-
-    #Now print out all lines:
-    for k, v in merged_dict.iteritems():
-        print v
+    to_f = file(to_ffield)
+    sections = parse_sections(to_f)
+    atom_dict = atom_section_to_dict(sections['atom'])
+    #print atom_dict
+    bond_dict = bond_section_to_dict(sections['bond'])
+    #print bond_dict
+    #print bond_dict['1|1']
+    #print bond_dict['11|11']
     
-    print 'Total entries: '+str(len(merged_dict))
+    
 
+    ##Parse text into dict-line
+    #base_dict = text_to_dict(base_text.splitlines())
+    ##print str(len(base_dict))
+    #override_dict = text_to_dict(override_text.splitlines())
+
+    ##Merge dicts
+    #merged_dict = merge_dicts(base_dict, override_dict)
+
+    ##Now print out all lines:
+    #for k, v in merged_dict.iteritems():
+    #    print v
+    #
+    #print 'Total entries: '+str(len(merged_dict))
+
+def get_lines(f, start, end = None):
+    '''
+    Given a file stream and the start and end line numbers, returns an array
+    with those lines. NOTE: Lines start at 1.
+
+    @param f File stream
+    @param start Starting line number (int). Must be >= 1.
+    @param end Ending line number (int). If None, then selects to end of file.
+    @return array of strings The specified lines in an array.
+    '''
+    f.seek(0) #reset to start of file
+    #Error checking:
+    if start < 1:
+        raise ValueError
+    if end != None and end < start:
+        raise ValueError
+    
+    #Unlike iteration (which starts at 0), we denote line numbers as starting at
+    #1. Therefore, subtract 1 from start here: 
+    start -= 1
+    #The way islice works is that it selects to the line right before end.
+    #For instance, to select just the first line, we need to do islice(f, 1, 2).
+    #Therefore, we need to subtract 1 from end.
+   
+    lines = []
+    for i in itertools.islice(f, start, end):
+        lines.append(i)
+
+    return lines
+
+def get_line(f, num):
+    '''
+    Given a file stream and a line number, returns that line as a string.
+    NOTE: Lines start at 1.
+
+    @param f File stream
+    @param num Line number (int). Must be >= 1.
+    @return string Line as a string.
+    '''
+    return get_lines(f, num, num)[0]
+
+def parse_sections(f):
+    '''
+    Given a file stream for ffield, parses the file into sections (ie. general
+    parameters, atom params, angle params, etc.). Then returns a dictionary that
+    includes all the sections.
+
+    @param f File stream for ffield.
+    @return dict Dictionary of sections. The key's are the section labels.
+    '''
+    sections = {}
+
+    #Identifiers for each section:
+    #For general:
+    #39       ! Number of general parameters                                        
+    #For atom:
+    #12    ! Nr of atoms; cov.r; valency;a.m;Rvdw;Evdw;gammaEEM;cov.r2;#            
+    atom_regex = re.compile(r'! Nr of atoms')
+    #For bonds:
+    #58      ! Nr of bonds; Edis1;LPpen;n.u.;pbe1;pbo5;13corr;pbo6                  
+    bond_regex = re.compile(r'! Nr of bonds')
+    #For off-diagonal terms:
+    #22    ! Nr of off-diagonal terms; Ediss;Ro;gamma;rsigma;rpi;rpi2               
+    offdiag_regex = re.compile(r'! Nr of off-diagonal terms')
+    #For angle terms:
+    #83    ! Nr of angles;at1;at2;at3;Thetao,o;ka;kb;pv1;pv2                        
+    angle_regex = re.compile(r'! Nr of angles')
+    #For torsion terms:
+    #58    ! Nr of torsions;at1;at2;at3;at4;;V1;V2;V3;V2(BO);vconj;n.u;n            
+    torsion_regex = re.compile(r'! Nr of torsions')
+    #For hydrogen bonds:
+    #9    ! Nr of hydrogen bonds;at1;at2;at3;Rhb;Dehb;vhb1                         
+    hydrogen_regex = re.compile(r'! Nr of hydrogen bonds')
+
+    #Determine what lines each of these start
+    general_start = 2
+    f.seek(0)
+    for i, line in enumerate(f):
+        if atom_regex.search(line):
+            atom_start = i+1 #line num = iter + 1
+        elif bond_regex.search(line):
+            bond_start = i+1 #line num = iter + 1
+        elif offdiag_regex.search(line):
+            offdiag_start = i+1 #line num = iter + 1
+        elif angle_regex.search(line):
+            angle_start = i+1 #line num = iter + 1
+        elif torsion_regex.search(line):
+            torsion_start = i+1 #line num = iter + 1
+        elif hydrogen_regex.search(line):
+            hydrogen_start = i+1 #line num = iter + 1
+
+    #print atom_start
+    #print bond_start
+    #print offdiag_start
+    #print angle_start
+    #print torsion_start
+    #print hydrogen_start
+
+    #General section: Anything between general_start and atom_start.
+    sections['general'] = get_lines(f, general_start+1, atom_start-1)
+    #print sections['general'][0]
+
+    #Atom section: Since each atom entry is exactly four lines, and since the
+    #comment after the 'Nr atoms' specification line seems to follow this
+    #convention too, we will just skip four lines after the specification line.
+    sections['atom'] = get_lines(f, atom_start+4, bond_start-1)
+    #print sections['atom'][0]
+
+    #Bond section: Each bond entry is exactly two lines. Assuming that the
+    #specification line follows same convention.
+    sections['bond'] = get_lines(f, bond_start+2, offdiag_start-1)
+    #print sections['bond'][0]
+    
+    #Off-diagonal section: 
+    sections['offdiag'] = get_lines(f, offdiag_start+1, angle_start-1)
+    #print sections['offdiag'][0]
+
+    #Angle section: 
+    sections['angle'] = get_lines(f, angle_start+1, torsion_start-1)
+    #print sections['angle'][0]
+    
+    #Torsion section: 
+    sections['torsion'] = get_lines(f, torsion_start+1, hydrogen_start-1)
+    #print sections['torsion'][0]
+    
+    #Hydrogen Bond section: 
+    sections['hydrogen'] = get_lines(f, hydrogen_start+1, None) #To end of file
+    #print sections['hydrogen'][0]
+
+    return sections
+
+def atom_section_to_dict(lines):
+    '''
+    Given an array of lines for the atom section, parses the lines into a
+    dictionary where each key is the atom type (ie. 'C', 'H', etc.) and the
+    value are the four lines associated with each atom.
+
+    @param lines Array of strings that comprise the lines of the atom section.
+    @return dict Dictionary, each key is atom type; each value contains the
+                 associated four lines of that atom type.
+    '''
+    atom_dict = {}
+    
+    #Sample starting line:
+    # C    1.3817   4.0000  12.0000   1.8903   0.1838   0.9000   1.1341   4.0000     
+    start_regex = re.compile(r'^\s*([A-Za-z]+)\s+.+')
+    for i, line in enumerate(lines):
+        m = start_regex.match(line)
+        if m: #check if there is a match
+            entry = []
+            #Add the four lines to this array
+            entry.append(line)
+            entry.append(lines[i+1])
+            entry.append(lines[i+2])
+            entry.append(lines[i+3])
+            
+            #Now add to dictionary
+            atom_dict[m.group(1)] = entry
+
+    return atom_dict
+        
+def bond_section_to_dict(lines):
+    '''
+    Given an array of lines for the bond section, parses the lines into a
+    dictionary where each key is a representation of the bond where the smaller
+    atom number is always on the left (ie. '1|2', '3|5', '4|15', etc.), and the
+    value are the two lines associated with each bond.
+
+    @param lines Array of strings that comprise the lines of the bond section.
+    @return dict Dictionary, each key is bond representation; each value
+                 contains the associated two lines of that bond type.
+    '''
+    bond_dict = {}
+    
+    #Sample two lines:
+    #1  1 158.2004  99.1897  78.0000  -0.7738  -0.4550   1.0000  37.6117   0.4147  
+    #       0.4590  -0.1000   9.1628   1.0000  -0.0777   6.7268   1.0000   0.0000  
+    #To parse:
+    #The number of lines in 'lines' array MUST be even since each entry has two
+    #associated lines. Therefore, just skip by every two lines.
+    assert len(lines) % 2 == 0 #eq 0 if even, 1 if odd
+    for i in xrange(0, len(lines), 2): #skip by two
+        #The first line begins with two numbers of atoms that are being bonded:
+        s = lines[i].split()
+        assert len(s) == 10 #sanity check
+        atom1 = int(s[0])
+        atom2 = int(s[1])
+
+        #Now we want to make the key label. We always write the smaller atom
+        #number to the left so that we don't end up with duplicates. For
+        #instance, between atom 1 and 15, we always make the key as: '1|15' and
+        #not as: '15|1'.
+        if atom1 <= atom2:
+            key = str(atom1)+'|'+str(atom2)
+        else:
+            key = str(atom2)+'|'+str(atom1)
+
+        #Put in dictionary
+        entry = []
+        entry.append(lines[i])
+        entry.append(lines[i+1])
+        bond_dict[key] = entry
+
+    return bond_dict
 
 def text_to_dict(text):
     dict = {}
